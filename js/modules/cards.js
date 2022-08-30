34b41f4df53d19b1e9a0eda45ea76b188ce2f806
Akhad <abdurazakovahad@gmail.com>
2022-08-30 09:05:15 +0300

    Webpack: Building JavaScript Modules

---
+++ js/modules/cards.js
@@ -0,0 +1,63 @@
+function cards() {
+    // Cards
+
+    class MenuCard {
+        constructor(img, alt, title, text, price, parentSelector, ...classes) {
+            this.img = img;
+            this.alt = alt;
+            this.title = title;
+            this.text = text;
+            this.price = price;
+            this.classes = classes;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 37;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price *= this.transfer; 
+        }
+
+        render() {
+            const elem = document.createElement('div');
+
+            if (this.classes.length === 0) {
+                this.elem = 'menu__item';
+                elem.classList.add(this.elem);
+            } else {
+                this.classes.forEach(className => elem.classList.add(className)); 
+            }
+
+            elem.innerHTML = `
+                <img src=${this.img} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.text}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            `;
+
+            this.parent.append(elem);
+        }
+    }
+    const getResource = async (url) => {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    };
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, alt, title, descr, price}) => {
+                new MenuCard(img, alt, title, descr, price, '.menu .container').render();
+            });
+        });
+}
+
+module.exports = cards;
